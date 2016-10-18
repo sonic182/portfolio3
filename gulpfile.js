a63d8fdd1b24996d56750bd790b211bfa83cf6a4
Johanderson <johander1822@gmail.com>
2016-10-18 15:18:05 +0200

    node resolve rollup

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp       = require('gulp'),
     rollup     = require('gulp-rollup'),
     sourcemaps = require('gulp-sourcemaps'),
     babel = require('gulp-babel'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    nodeResolve = require('rollup-plugin-node-resolve');
 
 gulp.task('scripts', () => {
   gulp.src('./assets/**/*.js')
@@ -12,6 +13,39 @@ gulp.task('scripts', () => {
       // transform the files here.
       .pipe(rollup({
         entry: './assets/js/main.js',
+        allowRealFiles: true,
+        plugins: [
+          nodeResolve({
+           // use "jsnext:main" if possible
+           // – see https://github.com/rollup/rollup/wiki/jsnext:main
+           jsnext: true,  // Default: false
+
+           // use "main" field or index.js, even if it's not an ES6 module
+           // (needs to be converted from CommonJS to ES6
+           // – see https://github.com/rollup/rollup-plugin-commonjs
+           main: true,  // Default: true
+
+           // if there's something your bundle requires that you DON'T
+           // want to include, add it to 'skip'. Local and relative imports
+           // can be skipped by giving the full filepath. E.g.,
+           // `path.resolve('src/relative-dependency.js')`
+           skip: [ 'some-big-dependency' ],  // Default: []
+
+           // some package.json files have a `browser` field which
+           // specifies alternative files to load for people bundling
+           // for the browser. If that's you, use this option, otherwise
+           // pkg.browser will be ignored
+           browser: true,  // Default: false
+
+           // not all files you want to resolve are .js files
+           extensions: [ '.js', '.json' ],  // Default: ['.js']
+
+           // whether to prefer built-in modules (e.g. `fs`, `path`) or
+           // local ones with the same names
+           preferBuiltins: false  // Default: true
+
+         })
+        ]
       }))
       .pipe(babel({
             presets: ['es2015']
