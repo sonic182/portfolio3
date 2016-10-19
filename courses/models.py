e3a5d83f1d51b56a2e3407dfeae90639c8492ca5
Johanderson <johander1822@gmail.com>
2016-10-19 11:09:13 +0200

    courses front

--- courses/models.py
+++ courses/models.py
@@ -7,6 +7,8 @@ class Course(models.Model):
     description = models.TextField()
     start_date = models.DateTimeField(blank=True, null=True)
     active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
@@ -15,3 +17,5 @@ class Student(models.Model):
     phone = models.CharField(max_length=100)
     birthdate = models.DateField(blank=True, null=True)
     courses = models.ForeignKey(Course)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
