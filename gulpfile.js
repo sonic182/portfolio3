a9dbab70d7c4dcb6df7e1aa9ccaf6eb43eb5a91d
Johanderson <johander1822@gmail.com>
2016-10-19 23:58:54 +0200

    minify assets

--- gulpfile.js
+++ gulpfile.js
@@ -5,57 +5,61 @@ var gulp       = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     babel = require('gulp-babel'),
     sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    pump = require('pump'),
     nodeResolve = require('rollup-plugin-node-resolve'),
     spawn = require('child_process').spawn;
 
 const static_path = './static';
+let rollupOpts = {
+  entry: './assets/js/main.js',
+  allowRealFiles: true,
+  context: 'window',
+  plugins: [
+    nodeResolve({
+     // use "jsnext:main" if possible
+     // – see https://github.com/rollup/rollup/wiki/jsnext:main
+     jsnext: true,  // Default: false
+
+     // use "main" field or index.js, even if it's not an ES6 module
+     // (needs to be converted from CommonJS to ES6
+     // – see https://github.com/rollup/rollup-plugin-commonjs
+     main: true,  // Default: true
+
+     // if there's something your bundle requires that you DON'T
+     // want to include, add it to 'skip'. Local and relative imports
+     // can be skipped by giving the full filepath. E.g.,
+     // `path.resolve('src/relative-dependency.js')`
+    //  skip: [ 'some-big-dependency' ],  // Default: []
+
+     // some package.json files have a `browser` field which
+     // specifies alternative files to load for people bundling
+     // for the browser. If that's you, use this option, otherwise
+     // pkg.browser will be ignored
+     browser: true,  // Default: false
+
+     // not all files you want to resolve are .js files
+     extensions: [ '.js', '.json' ],  // Default: ['.js']
+
+     // whether to prefer built-in modules (e.g. `fs`, `path`) or
+     // local ones with the same names
+    //  preferBuiltins: false  // Default: true
+
+   })
+  ]
+};
 
 gulp.task('scripts', () => {
-  gulp.src('./assets/**/*.js')
-    // .pipe(sourcemaps.init())
-      // transform the files here.
-      .pipe(rollup({
-        entry: './assets/js/main.js',
-        allowRealFiles: true,
-        context: 'window',
-        plugins: [
-          nodeResolve({
-           // use "jsnext:main" if possible
-           // – see https://github.com/rollup/rollup/wiki/jsnext:main
-           jsnext: true,  // Default: false
+  pump([
+    gulp.src('./assets/**/*.js'),
+    rollup(rollupOpts),
+    babel({
+      presets: ['es2015']
+    }),
+    uglify(),
+    gulp.dest(`${static_path}`)
+  ])
 
-           // use "main" field or index.js, even if it's not an ES6 module
-           // (needs to be converted from CommonJS to ES6
-           // – see https://github.com/rollup/rollup-plugin-commonjs
-           main: true,  // Default: true
-
-           // if there's something your bundle requires that you DON'T
-           // want to include, add it to 'skip'. Local and relative imports
-           // can be skipped by giving the full filepath. E.g.,
-           // `path.resolve('src/relative-dependency.js')`
-          //  skip: [ 'some-big-dependency' ],  // Default: []
-
-           // some package.json files have a `browser` field which
-           // specifies alternative files to load for people bundling
-           // for the browser. If that's you, use this option, otherwise
-           // pkg.browser will be ignored
-           browser: true,  // Default: false
-
-           // not all files you want to resolve are .js files
-           extensions: [ '.js', '.json' ],  // Default: ['.js']
-
-           // whether to prefer built-in modules (e.g. `fs`, `path`) or
-           // local ones with the same names
-          //  preferBuiltins: false  // Default: true
-
-         })
-        ]
-      }))
-      .pipe(babel({
-            presets: ['es2015']
-        }))
-    // .pipe(sourcemaps.write())
-    .pipe(gulp.dest(`${static_path}`));
 })
 
 gulp.task('fonts', () => {
@@ -69,7 +73,7 @@ gulp.task('fonts', () => {
 gulp.task('sass', () => {
  return gulp.src('./assets/scss/**/*.scss')
   // .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
   // .pipe(sourcemaps.write())
   .pipe(gulp.dest(`${static_path}`));
 })
