51e514288d9d1d06766e3d52d2ae14076ea7057b
Johanderson <johander1822@gmail.com>
2017-06-11 10:56:54 +0200

    dj database

--- portfolio3/settings.py
+++ portfolio3/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+
+import dj_database_url
+
 from django.utils.translation import ugettext_lazy as _
 
 environ = os.environ
@@ -110,12 +113,14 @@ WSGI_APPLICATION = 'portfolio3.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 if env == 'production':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db/production.sqlite3'),
-        }
-    }
+    DATABASES = {}
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': os.path.join(BASE_DIR, 'db/production.sqlite3'),
+    #     }
+    # }
 else:
     DATABASES = {
         'default': {
